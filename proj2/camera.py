fd68c9ff4c338e051bdb7a6abcf2cde1cfbb98db
Maciej Karabin <maciej.m.karabin@gmail.com>
2025-05-16 13:59:21 +0200

    final proj2?

--- proj2/camera.py
+++ proj2/camera.py
@@ -4,6 +4,177 @@ import tkinter as tk
 import numpy as np
 
 
+class Plane:
+    def __init__(self, point, normal):
+        self.point = np.array(point)
+        self.normal = np.array(normal)
+
+        self.normal = self.normal / np.linalg.norm(self.normal)
+
+    def classify_point(self, point):
+        # Oblicz iloczyn skalarny zeby wiedziec z ktorej strony plaszczyzny jest punkt
+        v = np.array(point) - self.point
+        dot = np.dot(v, self.normal)
+
+        if dot > 1e-6:
+            return "FRONT"
+        elif dot < -1e-6:
+            return "BACK"
+        else:
+            return "COPLANAR"
+
+    def classify_polygon(self, polygon):
+        # Gdzie lezy wielokat wzgledem plaszczyzny
+        front_count = 0
+        back_count = 0
+
+        for point in polygon:
+            classification = self.classify_point(point)
+            if classification == "FRONT":
+                front_count += 1
+            elif classification == "BACK":
+                back_count += 1
+
+        if front_count > 0 and back_count == 0:
+            return "FRONT"
+        elif back_count > 0 and front_count == 0:
+            return "BACK"
+        elif front_count == 0 and back_count == 0:
+            return "COPLANAR"
+        else:
+            return "SPANNING"
+
+    def split_polygon(self, polygon):
+        # Podziel wielokat przez plaszczyzne
+        # Zwraca front_poly, back_poly
+        front_points = []
+        back_points = []
+
+        # Dodaj kazdy punkt do odpowiedniej listy
+        for i in range(len(polygon)):
+            current = polygon[i]
+            next_point = polygon[(i + 1) % len(polygon)]
+
+            current_classification = self.classify_point(current)
+            next_classification = self.classify_point(next_point)
+
+            if current_classification == "FRONT" or current_classification == "COPLANAR":
+                front_points.append(current)
+            if current_classification == "BACK" or current_classification == "COPLANAR":
+                back_points.append(current)
+
+            # Jesli krawedz przecina plaszczyzne, stworz punkt przeciecia i dodaj do obu stron
+            if (current_classification == "FRONT" and next_classification == "BACK") or \
+                    (current_classification == "BACK" and next_classification == "FRONT"):
+                current_array = np.array(current)
+                next_array = np.array(next_point)
+                direction = next_array - current_array
+
+                # Oblicz punkt przeciecia
+                t = np.dot(self.point - current_array, self.normal) / np.dot(direction, self.normal)
+                intersection = current_array + t * direction
+
+                # Dodaj przeciecie do obu stron
+                front_points.append(tuple(intersection))
+                back_points.append(tuple(intersection))
+
+        return front_points if len(front_points) >= 3 else None, back_points if len(back_points) >= 3 else None
+
+
+class BSPNode:
+    def __init__(self, polygons=None):
+        self.plane = None
+        self.front = None
+        self.back = None
+        self.polygons = []
+
+        if polygons:
+            self.build(polygons)
+
+    def build(self, polygons):
+        if not polygons:
+            return
+
+        # Wez pierwszy wielokat zeby zdefiniowac plaszczyzne podzialu
+        first_poly = polygons[0]
+        # Wez trzy punkty z wielokata, zeby ogarnac plaszczyzne
+        p1, p2, p3 = first_poly[0], first_poly[1], first_poly[2]
+
+        # Oblicz wektor normalny przez iloczyn wektorowy
+        v1 = np.array(p2) - np.array(p1)
+        v2 = np.array(p3) - np.array(p1)
+        normal = np.cross(v1, v2)
+
+        # Stworz plaszczyzne podzialu
+        self.plane = Plane(p1, normal)
+
+        # Podziel wielokaty na front, back i coplanar
+        front_list = []
+        back_list = []
+
+        for poly in polygons:
+            classification = self.plane.classify_polygon(poly)
+
+            if classification == "FRONT":
+                front_list.append(poly)
+            elif classification == "BACK":
+                back_list.append(poly)
+            elif classification == "COPLANAR":
+                # Dodaj do wielokatow tego wezla
+                self.polygons.append(poly)
+            elif classification == "SPANNING":
+                # Podziel wielokat i dodaj do obu stron
+                front_poly, back_poly = self.plane.split_polygon(poly)
+                if front_poly:
+                    front_list.append(front_poly)
+                if back_poly:
+                    back_list.append(back_poly)
+
+        # Zbuduj rekurencyjnie poddrzewa
+        if front_list:
+            self.front = BSPNode(front_list)
+        if back_list:
+            self.back = BSPNode(back_list)
+
+    def get_visible_polygons(self, camera_position):
+        result = []
+
+        # Jesli nie ma plaszczyzny, to jest lisc w drzewie
+        if not self.plane:
+            return self.polygons
+
+        # Sprawdz po ktorej stronie plaszczyzny jest kamera
+        camera_side = self.plane.classify_point(camera_position)
+
+        # Przejdz drzewo w odpowiedniej kolejnosci w zaleznosci od pozycji kamery
+        if camera_side == "FRONT" or camera_side == "COPLANAR":
+            # Kamera jest z przodu lub na plaszczyznie
+            # Najpierw przerob tyl (dalej od kamery)
+            if self.back:
+                result.extend(self.back.get_visible_polygons(camera_position))
+
+            # Dodaj wielokaty tego wezla
+            result.extend(self.polygons)
+
+            # Potem przerob przod (blizej kamery)
+            if self.front:
+                result.extend(self.front.get_visible_polygons(camera_position))
+        else:
+            # Kamera jest z tylu
+            # Przerob przod najpierw
+            if self.front:
+                result.extend(self.front.get_visible_polygons(camera_position))
+
+            # Dodaj wielokaty tego wezla
+            result.extend(self.polygons)
+
+            # Potem przerob tyl
+            if self.back:
+                result.extend(self.back.get_visible_polygons(camera_position))
+
+        return result
+
+
 class CameraApp:
     def __init__(self, root):
         self.root = root
@@ -17,10 +188,17 @@ class CameraApp:
         self.focal_length = 500
         self.rotation_matrix = np.identity(3)
 
-        # wczytaj obiekty
+        with open('cube_polygons.json') as f:
+            self.polygons = json.load(f)
+            self.polygons = [[tuple(point) for point in polygon] for polygon in self.polygons]
+
+        # Wczytaj krawedzie do wyswietlania bez BSP
         with open('cube_data.json') as f:
             data = json.load(f)
-        self.cube_data = [tuple(tuple(point) for point in face) for face in data]
+        self.cube_data = [(tuple(p1), tuple(p2)) for p1, p2 in data]
+
+        # Zbuduj drzewo BSP
+        self.bsp_tree = BSPNode(self.polygons)
 
         grid_center = np.array([4.0, 4.0, 4.0])
         direction = grid_center - self.position
@@ -52,8 +230,14 @@ class CameraApp:
         self.root.bind('<g>', lambda e: self.roll_counter_clockwise())
         self.root.bind('<h>', lambda e: self.zoom_in())
         self.root.bind('<j>', lambda e: self.zoom_out())
+        self.root.bind('<b>', lambda e: self.toggle_bsp())
 
+        self.use_bsp = True
         self.draw_controls()
+        self.redraw()
+
+    def toggle_bsp(self):
+        self.use_bsp = not self.use_bsp
         self.redraw()
 
     def get_view_vectors(self):
@@ -152,41 +336,85 @@ class CameraApp:
         y_proj = (y / z) * self.focal_length
         return 400 + x_proj, 300 - y_proj
 
+    def calculate_polygon_normal(self, polygon):
+        if len(polygon) < 3:
+            return np.array([0, 0, 0])
+
+        # Oblicz wektor normalny dla wielokata
+        v1 = np.array(polygon[1]) - np.array(polygon[0])
+        v2 = np.array(polygon[2]) - np.array(polygon[0])
+        normal = np.cross(v1, v2)
+
+        return normal / np.linalg.norm(normal) if np.linalg.norm(normal) > 0 else normal
+
+    def is_polygon_facing_camera(self, polygon):
+        normal = self.calculate_polygon_normal(polygon)
+        centroid = np.mean([np.array(p) for p in polygon], axis=0)
+        view_vector = centroid - self.position
+
+        # Jesli iloczyn skalarny jest ujemny, wielokat jest skierowany w strone kamery
+        return np.dot(normal, view_vector) < 0
+
     def redraw(self):
         self.canvas.delete("all")
-        faces_with_depth = []
-        for face in self.cube_data:
-            points = []
-            depths = []
-            skip = False
-            for point in face:
-                translated = np.array(point) - self.position
-                rotated = self.rotation_matrix.T @ translated
-                x_rot, y_rot, z_rot = rotated
-                if z_rot <= 0:
-                    skip = True
-                    break
-                depths.append(z_rot)
-                x_proj = (x_rot / z_rot) * self.focal_length
-                y_proj = (y_rot / z_rot) * self.focal_length
-                screen_x = 400 + x_proj
-                screen_y = 300 - y_proj
-                points.append((screen_x, screen_y))
-            if not skip:
-                avg_depth = sum(depths) / len(depths)
-                faces_with_depth.append((avg_depth, points))
-        # Sort by depth (descending)
-        faces_with_depth.sort(key=lambda x: -x[0])
-        for depth, points in faces_with_depth:
-            self.canvas.create_polygon(points, fill='white', outline='black')
+
+        if self.use_bsp:
+            # Uzyj drzewa BSP do eliminacji powierzchni zaslonietych
+            visible_polygons = self.bsp_tree.get_visible_polygons(tuple(self.position))
+
+            # Narysuj widoczne wielokaty
+            for poly in visible_polygons:
+                # Sprawdz, czy wielokat jest skierowany w strone kamery - inaczej go nie rysuj
+                if not self.is_polygon_facing_camera(poly):
+                    continue
+
+                points_2d = []
+                valid = True
+
+                for point in poly:
+                    proj = self.project_point(point)
+                    if not proj:
+                        valid = False
+                        break
+                    points_2d.append(proj)
+
+                if valid and len(points_2d) >= 3:
+                    flat_points = [coord for point in points_2d for coord in point]
+
+                    gray_value = 200
+
+                    color = f'#{gray_value:02x}{gray_value:02x}{gray_value:02x}'
+
+                    # Renderuj tylko w trybie solid - pelne wielokaty
+                    self.canvas.create_polygon(flat_points, fill=color, outline='black')
+        else:
+            # krawedzie bez BSP - tylko linie
+            for edge in self.cube_data:
+                p1 = self.project_point(edge[0])
+                p2 = self.project_point(edge[1])
+                if p1 and p2:
+                    self.canvas.create_line(p1[0], p1[1], p2[0], p2[1], fill='black')
+
         self.draw_controls()
 
     def draw_controls(self):
-        controls = """Controls:
-        W/S - Move forward/backward
-        A/D - Strafe left/right
-        Q/E - Move up/down
-        F/G - Roll clockwise/counter
-        Arrows - Look around
-        H/J - Zoom in/out"""
+        controls = """Kontrolki:
+        W/S - Ruch przód/tył
+        A/D - Strafe lewo/prawo
+        Q/E - Góra/dół
+        F/G - Obrót w osi Z
+        Strzałki - Rozglądanie
+        H/J - Przybliż/oddal
+        B - Przełącz BSP"""
+
+        bsp_status = "BSP: ON" if self.use_bsp else "BSP: OFF"
+
         self.canvas.create_text(10, 10, text=controls, anchor='nw', fill='black')
+        self.canvas.create_text(400, 10, text=bsp_status, anchor='n', fill='blue')
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("3D Camera with BSP Tree")
+    app = CameraApp(root)
+    root.mainloop()
--- proj2/generate_cubes.py
+++ proj2/generate_cubes.py
@@ -2,14 +2,13 @@ import json
 
 
 def save_cube_data(filename):
-    cube_data = generate_cube_grid()
+    polygons = generate_cube_grid()
 
     serializable = []
-    for face in cube_data:
-        serializable_face = []
-        for point in face:
-            serializable_face.append([float(coord) for coord in point])
-        serializable.append(serializable_face)
+    for polygon in polygons:
+        serializable.append([
+            [float(coord) for coord in point] for point in polygon
+        ])
 
     with open(filename, 'w') as f:
         json.dump(serializable, f)
@@ -17,9 +16,10 @@ def save_cube_data(filename):
 
 def generate_cube_grid():
     size = 3
-    cubes = []
+    polygons = []
     spacing = 1
     cube_size = 1.2
+
     for x in range(size):
         for y in range(size):
             for z in range(size):
@@ -27,49 +27,78 @@ def generate_cube_grid():
                 base_y = y * (cube_size + spacing)
                 base_z = z * (cube_size + spacing)
 
-                faces = [
-                    # Front face
-                    (
-                        (base_x, base_y, base_z),
-                        (base_x + cube_size, base_y, base_z),
-                        (base_x + cube_size, base_y + cube_size, base_z),
-                        (base_x, base_y + cube_size, base_z)
-                    ),
-                    # Back face
-                    (
-                        (base_x, base_y, base_z + cube_size),
-                        (base_x + cube_size, base_y, base_z + cube_size),
-                        (base_x + cube_size, base_y + cube_size, base_z + cube_size),
-                        (base_x, base_y + cube_size, base_z + cube_size)
-                    ),
-                    # Top face
-                    (
-                        (base_x, base_y + cube_size, base_z),
-                        (base_x + cube_size, base_y + cube_size, base_z),
-                        (base_x + cube_size, base_y + cube_size, base_z + cube_size),
-                        (base_x, base_y + cube_size, base_z + cube_size)
-                    ),
-                    # Bottom face
-                    (
-                        (base_x, base_y, base_z),
-                        (base_x + cube_size, base_y, base_z),
-                        (base_x + cube_size, base_y, base_z + cube_size),
-                        (base_x, base_y, base_z + cube_size)
-                    ),
-                    # Left face
-                    (
-                        (base_x, base_y, base_z),
-                        (base_x, base_y, base_z + cube_size),
-                        (base_x, base_y + cube_size, base_z + cube_size),
-                        (base_x, base_y + cube_size, base_z)
-                    ),
-                    # Right face
-                    (
-                        (base_x + cube_size, base_y, base_z),
-                        (base_x + cube_size, base_y + cube_size, base_z),
-                        (base_x + cube_size, base_y + cube_size, base_z + cube_size),
-                        (base_x + cube_size, base_y, base_z + cube_size)
-                    )
-                ]
-                cubes.extend(faces)
-    return cubes
+                v0 = (base_x, base_y, base_z)
+                v1 = (base_x + cube_size, base_y, base_z)
+                v2 = (base_x + cube_size, base_y + cube_size, base_z)
+                v3 = (base_x, base_y + cube_size, base_z)
+                v4 = (base_x, base_y, base_z + cube_size)
+                v5 = (base_x + cube_size, base_y, base_z + cube_size)
+                v6 = (base_x + cube_size, base_y + cube_size, base_z + cube_size)
+                v7 = (base_x, base_y + cube_size, base_z + cube_size)
+
+                polygons.append([v0, v3, v2, v1])
+                polygons.append([v4, v5, v6, v7])
+                polygons.append([v0, v1, v5, v4])
+                polygons.append([v1, v2, v6, v5])
+                polygons.append([v2, v3, v7, v6])
+                polygons.append([v3, v0, v4, v7])
+
+    return polygons
+
+
+def generate_edges_from_polygons(polygons):
+    edges = set()
+
+    for poly in polygons:
+        for i in range(len(poly)):
+            v1 = poly[i]
+            v2 = poly[(i + 1) % len(poly)]
+
+            if v1 < v2:
+                edge = (v1, v2)
+            else:
+                edge = (v2, v1)
+
+            edges.add(edge)
+
+    return list(edges)
+
+
+def save_both_formats(filename_polygons, filename_edges):
+    polygons = generate_cube_grid()
+    edges = generate_edges_from_polygons(polygons)
+
+    serializable_polygons = []
+    for polygon in polygons:
+        serializable_polygons.append([
+            [float(coord) for coord in point] for point in polygon
+        ])
+
+    with open(filename_polygons, 'w') as f:
+        json.dump(serializable_polygons, f)
+
+    serializable_edges = []
+    for edge in edges:
+        serializable_edges.append([
+            [float(coord) for coord in edge[0]],
+            [float(coord) for coord in edge[1]]
+        ])
+
+    with open(filename_edges, 'w') as f:
+        json.dump(serializable_edges, f)
+
+
+if __name__ == "__main__":
+    save_cube_data("cube_polygons.json")
+
+    edges_polygons = generate_edges_from_polygons(generate_cube_grid())
+
+    serializable_edges = []
+    for edge in edges_polygons:
+        serializable_edges.append([
+            [float(coord) for coord in edge[0]],
+            [float(coord) for coord in edge[1]]
+        ])
+
+    with open("cube_data.json", 'w') as f:
+        json.dump(serializable_edges, f)
